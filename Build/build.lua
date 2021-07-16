42e89a0ed4263bceb691ef9501f2207462df1f77
astrialnight <77846505+astriaInight@users.noreply.github.com>
2021-07-15 21:04:00 -0400

    Update build.lua
    Added api modules downloading :)

--- Build/build.lua
+++ Build/build.lua
@@ -14,7 +14,7 @@ local sources = {
 	license = nil
 }
 local http = game:GetService("HttpService")
-local requestWait = 4
+local requestWait = 0.5
 local templateScript = "-- RobloxAPI --\n-- %s\n\n--[[\n%s\n]]\n\n%s"
 
 -- Functions
@@ -33,6 +33,24 @@ wait(requestWait)
 
 sources.license = http:GetAsync(urls.license)
 print("Loaded license.")
+
+-- Load API modules
+warn("Downloading APIs...")
+
+wait(requestWait)
+
+local apis = http:JSONDecode(http:GetAsync("https://api.github.com/repos/astriaInight/RobloxAPI/contents/APIs?ref=main"))
+for _, api in pairs(apis) do
+	local apiModule = Instance.new("ModuleScript")
+	apiModule.Name = api.name:gsub(".lua", "") -- Removes .lua from name
+	
+	wait(requestWait)
+	
+	apiModule.Source = http:GetAsync(api.download_url) -- Actually the raw data in this case
+	apiModule.Parent = module
+	
+	print("Loaded API: " .. apiModule.Name .. ".")
+end
 
 -- Compile
 warn("Compiling...")
